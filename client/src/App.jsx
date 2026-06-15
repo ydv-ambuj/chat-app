62621447f5560d9dcee7dde3e995146c038cfd40
Ambuj Yadav <ambujyadav1984@gmail.com>
2026-06-15 21:23:59 +0530

    server.js updated for Vercel

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -9,7 +9,7 @@ import { AuthContext } from '../context/AuthContext'
 const App = () => {
   const { authUser }= useContext(AuthContext)
   return (
-    <div className="bg-[url('./src/assets/bgImage.svg')]
+    <div className="bg-[url('/bgImage.svg')]
      bg-contain">
       <Toaster/>
       <Routes>
--- server/server.js
+++ server/server.js
@@ -55,6 +55,10 @@ app.use("/api/messages",messageRouter);
 //connect to mongodb
 await connectDB();
 
+if(process.env.NODE_ENV != "production"){
 const PORT =process.env.PORT || 5000;
 server.listen(PORT,()=>console.log(`server is running on ${PORT}`)
 );
+}
+// Export server for versal
+export default server;
